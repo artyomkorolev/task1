cc76a8492a7ea2edfb43d621c0987323ee7e1624
Artyom Korolev <40526124+artyomkorolev@users.noreply.github.com>
2022-03-13 20:42:19 +0300

    Add files via upload

---
+++ taskmarth1/src/ru/vsu/cs/korolevae/Main.java
@@ -0,0 +1,9 @@
+package ru.vsu.cs.korolevae;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = perevod.perevvod(10000,2,8);
+        System.out.println(i);
+    }
+}
---
+++ taskmarth1/src/ru/vsu/cs/korolevae/task.java
@@ -0,0 +1,42 @@
+package ru.vsu.cs.korolevae;
+
+class perevod{
+    public static int perv10 (int chislo, int sisschisl){
+     int sum = 0;
+     int ostatok=0;
+     int st=0;
+      while (chislo!= 0){
+          ostatok= chislo %10;
+          ostatok= (int) (ostatok * Math.pow(sisschisl,st));
+          sum= sum + ostatok;
+          ostatok=0;
+          st= st+1;
+          chislo=chislo/10;
+
+      }
+
+
+        return sum;
+    }
+    public static int pereviz10(int chisl0, int sisschisl){
+        int sum=0;
+        int ostatok= 0;
+        int st=0;
+        while (chisl0!=0){
+            ostatok= chisl0 % sisschisl;
+            sum= (int) (sum + ostatok*Math.pow(10,st));
+            st= st+1;
+            ostatok=0;
+            chisl0= chisl0/sisschisl;
+
+        }
+        return sum;
+    }
+    public static int perevvod(int chislo,int ssishod, int ssneed){
+        chislo= perv10(chislo,ssishod);
+        chislo=pereviz10(chislo,ssneed);
+        return chislo;
+
+    }
+
+}
