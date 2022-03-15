ae0879ac94f41cbd2af075ceed42f3b5ae606c20
Artyom Korolev <40526124+artyomkorolev@users.noreply.github.com>
2022-03-16 00:23:11 +0300

    Update Main.java

--- taskmarth1/src/ru/vsu/cs/korolevae/Main.java
+++ taskmarth1/src/ru/vsu/cs/korolevae/Main.java
@@ -1,9 +1,19 @@
 package ru.vsu.cs.korolevae;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         perevod r= new perevod();
-        System.out.println(r.perevvod("24526",7,10));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число");
+        String c = sc.nextLine();
+        System.out.println("Введите исходную систему исчисления");
+        int a = sc.nextInt();
+        System.out.println("В какую систему счисления перевести?");
+        int b = sc.nextInt();
+        System.out.println(r.perevvod(c,a,b));
     }
 }
