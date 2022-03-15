1d43c88fd71eee702c5d4f1ec6ec1cb8c4b1548c
Artyom Korolev <40526124+artyomkorolev@users.noreply.github.com>
2022-03-15 19:31:03 +0300

    Update Main.java

--- taskmarth1/src/ru/vsu/cs/korolevae/Main.java
+++ taskmarth1/src/ru/vsu/cs/korolevae/Main.java
@@ -3,7 +3,8 @@ package ru.vsu.cs.korolevae;
 public class Main {
 
     public static void main(String[] args) {
-        int i = perevod.perevvod(10000,2,8);
+        perevod r= new perevod();
+        int i = perevod.perevvod(23,2,8);
         System.out.println(i);
     }
 }
