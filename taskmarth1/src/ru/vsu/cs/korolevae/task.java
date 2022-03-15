6b725987ed682c235ba7f51cb897966b914c12f4
Artyom Korolev <40526124+artyomkorolev@users.noreply.github.com>
2022-03-16 00:15:55 +0300

    Update task.java

--- taskmarth1/src/ru/vsu/cs/korolevae/task.java
+++ taskmarth1/src/ru/vsu/cs/korolevae/task.java
@@ -1,42 +1,109 @@
 package ru.vsu.cs.korolevae;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 class perevod{
-    public  int perv10 (int chislo, int sisschisl){
-     int sum = 0;
-     int ostatok=0;
-     int st=0;
-      while (chislo!= 0){
-          ostatok= chislo %10;
-          ostatok= (int) (ostatok * Math.pow(sisschisl,st));
-          sum= sum + ostatok;
-          ostatok=0;
-          st= st+1;
-          chislo=chislo/10;
+    public  int perv10 (String chislo, int sisschisl){
 
-      }
+        List<String> list = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+
+        String[] str_arr = chislo.split("");
+            for (String item: str_arr){
+
+                if(item.equals("A")){
+                    list1.add(10);continue;
+                }
+                if(item.equals("B")){
+                    list1.add(11);continue;
+                }
+                if(item.equals("C")){
+                    list1.add(12);continue;
+                }
+                if(item.equals("D")){
+                    list1.add(13);continue;
+                }
+                if(item.equals("E")){
+                    list1.add(14);continue;
+                }
+                if(item.equals("F")){
+                    list1.add(15);
+                    continue;
+                }
 
 
-        return sum;
+                list1.add(Integer.parseInt(item));
+
+            }
+
+            int summa = 0;
+            for(int r = list1.size()-1;r>=0;r--){
+                summa+= list1.get(r) * Math.pow(sisschisl,list1.size()-1-r);
+            }
+
+        System.out.println(summa);
+         return summa;
+
     }
-    public  int pereviz10(int chisl0, int sisschisl){
-        int sum=0;
-        int ostatok= 0;
-        int st=0;
-        while (chisl0!=0){
-            ostatok= chisl0 % sisschisl;
-            sum= (int) (sum + ostatok*Math.pow(10,st));
-            st= st+1;
-            ostatok=0;
-            chisl0= chisl0/sisschisl;
 
+    public  String pereviz10(int chislo, int sisschisl){
+        List<String> list = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        int ostatok = 0;
+        while (chislo != 0) {
+            ostatok = chislo % sisschisl;
+            list1.add(ostatok);
+            ostatok = 0;
+            chislo = chislo / sisschisl;
         }
-        return sum;
-    }
-    public  int perevvod(int chislo,int ssishod, int ssneed){
-        chislo= perv10(chislo,ssishod);
-        chislo=pereviz10(chislo,ssneed);
-        return chislo;
+
+        int size= list1.size()-1;
+        for (int r=size;r>=0;r--){
+            list2.add(list1.get(r));
+        }
+
+        for (Integer item:list2){
+            if (item>=10){
+                if (item== 12 )
+                    list.add("C");
+                if (item== 10 )
+                    list.add("A");
+                if (item== 11 )
+                    list.add("B");
+                if (item== 13 )
+                    list.add("D");
+                if (item== 14 )
+                    list.add("E");
+                if (item== 15 )
+                    list.add("F");}
+            else
+                list.add(String.valueOf(item));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String i:list){
+            sb.append(i);
+        }
+
+        return sb.toString();
+
+
 
     }
 
-}
+    public String perevvod(String chislo,int ssishod, int ssneed){
+        int chisl= perv10(chislo,ssishod);
+        String chisle= pereviz10(chisl,ssneed);
+        return chisle;}
+
+    }
+
+
+
+
+
+
